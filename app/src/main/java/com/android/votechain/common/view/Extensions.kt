aaeec7e3ef25f75e596c85f51f4585f860258330
ariana <arianarsdev@gmail.com>
2016-06-21 02:36:51 -0500

    Viewpager color animation was created

--- app/src/main/java/com/android/votechain/common/view/Extensions.kt
+++ app/src/main/java/com/android/votechain/common/view/Extensions.kt
@@ -1,5 +1,8 @@
 package com.android.votechain.common.view
 
+import android.app.Activity
+import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,4 +10,8 @@ import android.view.ViewGroup
 
 fun ViewGroup.inflate(layoutResId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutResId, this, attachToRoot)
+}
+
+fun Activity.getCompatColor(colorRes:Int): Int{
+    return ContextCompat.getColor(this, colorRes)
 }
--- app/src/main/java/com/android/votechain/tutorial/TutorialActivity.kt
+++ app/src/main/java/com/android/votechain/tutorial/TutorialActivity.kt
@@ -1,8 +1,11 @@
 package com.android.votechain.tutorial
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.support.v4.view.ViewPager
 import com.android.votechain.R
 import com.android.votechain.common.view.BaseActivity
+import com.android.votechain.common.view.getCompatColor
 import me.relex.circleindicator.CircleIndicator
 
 class TutorialActivity : BaseActivity() {
@@ -24,6 +27,10 @@ class TutorialActivity : BaseActivity() {
         circles
     }
 
+    val colorAnimator by lazy {
+        ValueAnimator.ofObject(ArgbEvaluator(), getCompatColor(R.color.purple), getCompatColor(R.color.indigo), getCompatColor(R.color.teal), getCompatColor(R.color.orange))
+    }
+
     override fun getLayout(): Int {
         return R.layout.activity_tutorial
     }
@@ -31,5 +38,18 @@ class TutorialActivity : BaseActivity() {
     override fun initView() {
         super.initView()
         pager
+        colorAnimator.addUpdateListener { animator -> pager.setBackgroundColor(animator.getAnimatedValue() as Int) }
+        colorAnimator.setDuration(((4 - 1) * 10000000000).toLong());
+        pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                colorAnimator.currentPlayTime = ((positionOffset + position)* 10000000000).toLong()
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+        })
     }
 }
