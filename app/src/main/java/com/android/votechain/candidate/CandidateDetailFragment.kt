e0894cd76677f2460c19d61e60b52271d4404f77
ariana <arianarsdev@gmail.com>
2016-06-21 08:56:50 -0500

    Kotlin android extensions fixed

---
+++ app/src/main/java/com/android/votechain/candidate/CandidateDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.android.votechain.candidate
+
+import com.android.votechain.R
+import com.android.votechain.common.view.BaseFragment
+
+
+class CandidateDetailFragment : BaseFragment() {
+
+    override fun getFragmentLayout(): Int {
+        return R.layout.fragment_candidate_detail
+    }
+}
